cfdb797609215e4fa2aa46a1e065d7d81e8edf4f
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-04-26 21:02:58 +0100

    my commit

--- src/components/RightSide.js
+++ src/components/RightSide.js
@@ -18,7 +18,7 @@ const RightSide = () => {
     >
       <div className="absolute inset-y-0 right-0 w-28 flex items-center justify-center">
         <ArrowForwardIcon
-          className="absolute bg-[#635dad] rounded-l-full w-[50px] hover:w-[55px] transition-all duration-500 h-[40px] p-[8px] top-1/2 right-0 transform -translate-y-1/2 text-white cursor-pointer"
+          className="absolute z-50 bg-[#635dad] rounded-l-full w-[50px] hover:w-[55px] transition-all duration-500 h-[40px] p-[8px] top-1/2 right-0 transform -translate-y-1/2 text-white cursor-pointer"
           onClick={() => setSlideRight(!slideRight)}
         />
       </div>
