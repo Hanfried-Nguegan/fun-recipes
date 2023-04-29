e0ee2bf975702c19ec2a6f1540a97cfdcc1ba215
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-04-29 14:48:30 +0100

    recipe commit

--- src/components/RecipesCardRight.js
+++ src/components/RecipesCardRight.js
@@ -15,7 +15,7 @@ const RecipeCard = ({ image, title, timer, description }) => {
   };
   return (
     <div
-      className={`bg-white min-w-[140px] md:w-[180px] min-h-[180px] md:h-[220px] rounded-[10px] shadow-2xl cursor-pointer ${
+      className={`bg-white ml-4 min-w-[140px] md:w-[180px] min-h-[180px] md:h-[220px] rounded-[10px] shadow-2xl cursor-pointer ${
         isHover ? "scale-105" : ""
       } transition-all duration-500`}
       onMouseEnter={handleMouseEnter}
@@ -43,7 +43,7 @@ const RecipeCard = ({ image, title, timer, description }) => {
 
 const RecipesCardRight = () => {
   return (
-    <div className="flex absolute bottom-[3%] left-[42%] md:left-[36%] sm:gap-10 gap-8">
+    <div className="flex absolute bottom-[3%] left-[42%] md:left-[36%] gap-8 ">
       {recipesDataRight.map((recipe, index) => (
         <RecipeCard key={recipe.title} index={index} {...recipe} />
       ))}
