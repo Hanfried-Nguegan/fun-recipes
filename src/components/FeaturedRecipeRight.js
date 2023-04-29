394684edd3c4d37702bc53a170b95c30cd4c9412
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-04-29 23:50:00 +0100

    featured recipe

--- src/components/FeaturedRecipeRight.js
+++ src/components/FeaturedRecipeRight.js
@@ -3,7 +3,7 @@ import { FeaturedDataRight } from "@/constants";
 
 const FeaturedCard = () => {
   return (
-    <div className="bg-white min-w-[300px] md:w-[400px] lg:w-[460px] min-h-[220px] md:h-[250px] lg:h-[270px] rounded-[10px] "></div>
+    <div className="bg-white min-w-[260px] md:w-[400px] lg:w-[460px] min-h-[220px] md:h-[250px] lg:h-[270px] rounded-[10px] "></div>
   );
 };
 
