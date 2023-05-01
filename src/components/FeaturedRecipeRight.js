e5cdfbe98b778bf38025d12072c11ee1d2b952c7
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2023-05-01 16:59:23 +0100

    recipe commit

--- src/components/FeaturedRecipeRight.js
+++ src/components/FeaturedRecipeRight.js
@@ -17,15 +17,15 @@ const FeaturedCard = ({ image, title, timer, ingredients }) => {
           />
         </div>
         <div className="flex flex-col w-[52%] h-full p-3">
-          <h1 className="textstyle flex-wrap font-bold text-[12px] md:text-[20px] lg:text-[25px]">
+          <h1 className="textstyle w-full flex-wrap font-bold text-[12px] md:text-[20px] lg:text-[25px]">
             {title}
           </h1>
           <ul className="mt-1 ">
-            <li className="text-[10px] md:text-[12px] lg:text-[14px] tracking-wider">
+            <li className="w-full text-[10px] md:text-[12px] lg:text-[14px] tracking-wider">
               {ingredients}
             </li>
           </ul>
-          <div className="flex items-center justify-between mt-2 ">
+          <div className="flex w-full items-center justify-between mt-2 ">
             <p className="flex flex-col items-center bg-[#625eaf] rounded-full text-white p-2 text-xs leading-3 font-semibold">
               {timer}
               <br /> <span className="text-white">min</span>
